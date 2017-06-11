560e7a8037305263f029718bb91d847b873a96f0
Capelli <the.1man@hotmail.com>
2017-06-11 13:14:38 +0200

    merge 1

--- test/test0.lua
+++ test/test0.lua
@@ -9,19 +9,18 @@
 --AddMesh({{1,2,3},{4,5,6},{7,8,9}})
 
 io.write("   AddBox(): \n")
-addBox({-20,10,30}, 4, "testBox1")
---AddBox({-20,10,30}, 4)
---AddBox({-20,30}, 4, "testBox1")
---AddBox({-20,10,30})
---AddBox({-20,,10,30})
+--addBox({-20,10,30}, 4, "testBox1")
+--addBox({-20,10,30}, 4)
+--addBox({-20,30}, 4, "testBox1")
+--addBox({-20,10,30})
+--addBox({-20,,10,30})
 
 io.write("\n   AddMesh(): \n")
-addMesh({{50,50,50}, {50,70,50}, {50,60,80}})
---AddMesh({"orange"},{},{}) -- Error: non-numeric coordinates
---AddMesh({{1,2,3,4},{1,1,1},{2,2,2}}) -- Error: number of components
---AddMesh({{1,2,3}}) -- Error: not a valid number of vertices
+--addMesh({{50,50,50}, {50,70,50}, {50,60,80}})
+--addMesh({"orange"},{},{}) -- Error: non-numeric coordinates
+--addMesh({{1,2,3,4},{1,1,1},{2,2,2}}) -- Error: number of components
+addMesh({{1,2,3}}) -- Error: not a valid number of vertices
 
 camera({50,50,50}, {0,0,0})
 
-for k,v in pairs(getNodes()) do print(k,v) end
---for k,v in pairs(getNodes()) do for kk,vv in pairs(v) do print(k,kk,vv) end end
+for k,v in pairs(getNodes()) do for kk,vv in pairs(v) do print(k,kk,vv) end end
